1a6c1f9795fe889858437de29c9ffb3ff4a97cb5
Olivia R. Lynn <olynn@andrew.cmu.edu>
2023-07-11 13:25:03 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,6 @@ version = ".".join(release.split(".")[:2])
 
 extensions = ["sphinx.ext.mathjax", "sphinx.ext.napoleon", "sphinx.ext.viewcode"]
 
-extensions.append("autoapi.extension")
 extensions.append("nbsphinx")
 
 templates_path = []

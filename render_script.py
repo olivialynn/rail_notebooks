7a87cbc3607e3cc0638b3f9c3c0463431aa10871
Olivia R. Lynn <olynn@andrew.cmu.edu>
2023-07-19 09:43:55 -0400

    Update render_script.py

--- render_script.py
+++ render_script.py
@@ -4,9 +4,8 @@ def render_nb():
     status = {}
 
     #for nb_file in ['docs/notebooks/core_examples/intro_notebook.ipynb', 'docs/notebooks/core_examples/FluxtoMag_and_Deredden_example.ipynb']:
-    #for nb_file in ['rail/examples/core_examples/FluxtoMag_and_Deredden_example.ipynb']:
-    
-    for nb_file in glob.glob("rail/examples/*_examples/*.ipynb"):
+    #for nb_file in glob.glob("rail/examples/*_examples/*.ipynb"):
+    for nb_file in ['rail/examples/core_examples/FluxtoMag_and_Deredden_example.ipynb']:
         print(f'\nnb_file: {nb_file}')
         subdir = os.path.dirname(nb_file).split('/')[-1]
         basename = os.path.splitext(os.path.basename(nb_file))[0]

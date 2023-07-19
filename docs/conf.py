33b617d5fc01796ed5bbdd9c54b8d6cf5f4cd29e
Olivia R. Lynn <olynn@andrew.cmu.edu>
2023-07-19 11:31:38 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,15 @@ autoapi_add_toc_tree_entry = False
 autoapi_member_order = "bysource"
 """
 
+
+# -- CSS ---------------------------------------------------------------------
 html_theme = "sphinx_rtd_theme"
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
 html_css_files = [
-  "notebook_cell_background.css",
+    'css/cell_background.css',
 ]
